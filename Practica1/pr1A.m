7fcedc2b08237a9d484b5051b4aa6a651d6be72a
Mario Gonzalez <mario.gonzalez.a2599@gmail.com>
2021-08-24 22:24:22 -0500

    Codigo Simplificado

--- Practica1/pr1A.m
+++ Practica1/pr1A.m
@@ -5,72 +5,68 @@ close all
 warning off all %Apaga los warnings
 %--------------------------------------------------------------------------
 %Variables
-fprintf('¿Cuántas clases quieres? ')
-numClases=input('')
-%numClases=2;
-fprintf('¿Cuántos representates por clase quieres? ')
-numRepresentantes=input('')
-numRepresentantes=200;
+numClases=input('¿Cuántas clases quieres?: ');
+numRepresentantes=input('¿Cuántos representates por clase quieres?: ');
+
 %Centroides
 centroides=zeros(numClases,1);
+fprintf('\n');
 for aux=1:numClases
-    fprintf('Escribe el centroide de la Clase %d: ',aux)
-	aX=input('')
+    fprintf('Escribe el centroide de la Clase %d: ',aux);
+    aX=input('');
     centroides(aux,1)=aX;
 end
-fprintf('Centroides:\n')
-disp(centroides)
+fprintf('Centroides:\n');
+disp(centroides');
 
 %Disperción
-%
-% No me acuerdo de esto
-%
+disperciones=zeros(numClases,1);
+fprintf('\n');
+for aux=1:numClases
+    fprintf('Escribe la dispercion de la Clase %d: ',aux);
+    aX=input('');
+    disperciones(aux,1)=aX;
+end
+fprintf('Disperciones:\n');
+disp(disperciones');
 
 %Crear clases:
-clases=zeros(2*numClases,numRepresentantes);
-medias=zeros(2*numClases,1);
+clases=zeros(2,numRepresentantes,numClases);
+medias=zeros(2,numClases)
 aux1=1;
 for aux=1:numClases
-    cAux=randn(2,numRepresentantes)+centroides(aux,1);
-    clases(aux1:aux1+1,1:numRepresentantes)=cAux;
-    mediaAux=(1/numRepresentantes)*[sum(cAux(1,:)) ; sum(cAux(2,:))]
-    medias(aux1:aux1+1,1)=mediaAux
-    aux1=aux1+2;
+    cAux=(randn(2,numRepresentantes)+centroides(aux,1))*disperciones(aux,1);
+    clases(:,:,aux)=cAux;
+    medias(:,aux)=mean(clases(:,:,aux),2);
 end
-fprintf('Clases:\n')
-disp(clases)
+fprintf('Clases:\n');
+disp(clases);
+fprintf('Medias:\n');
+disp(medias);
 
 %--------------------------------------------------------------------------
+
 %Entrada del vector desconocido
 fprintf('Entrada del Vector desconocido: \n')
-vecX=input('    Coordenada en X: ')
-vecY=input('    Coordenada en Y: ')
-vecDesc=[vecX;vecY]
-
-
-
-
-
+vecX=input('    Coordenada en X: ');
+vecY=input('    Coordenada en Y: ');
+vecDesc=[vecX;vecY];
 
 %GRAFICANDO CLASES
 figure(1)
-plot(vecDesc(1,:),vecDesc(2,:),'ko','MarkerSize',10,'MarkerFaceColor','k','DisplayName','Vector Desc')
+plot(vecDesc(1,:),vecDesc(2,:),'ko','MarkerSize',6,'MarkerFaceColor','k','DisplayName','Vector Desc')
 grid on
 hold on
-colores={'r','g','c','m','y'};
-cont=1;
-for aux=1:2:(2*numClases)
-    cstr=strcat('Clase',{' '},string(cont));
+for aux=1:numClases
+    cstr=strcat('Clase',{' '},string(aux));
 	%plot(clases(aux,:),clases(aux+1,:),'o','MarkerSize',10,'MarkerFaceColor','r','DisplayName',cstr)
-    plot(clases(aux,:),clases(aux+1,:),'.','MarkerSize',10,'DisplayName',cstr)
-    cont=cont+1;
+    plot(clases(1,:,aux),clases(2,:,aux),'.','MarkerSize',15,'DisplayName',cstr,'color',rand(1,3))
 end
-cont=1;
-for aux=1:2:(2*numClases)
-    mstr=strcat('Media',{' '},string(cont));
+
+for aux=1:numClases
+    mstr=strcat('Media',{' '},string(aux));
 	%plot(medias(aux,1),medias(aux+1,1),'d','MarkerSize',12,'MarkerFaceColor','b','MarkerEdgeColor','k','DisplayName',mstr)
-    plot(medias(aux,1),medias(aux+1,1),'+','MarkerSize',12,'DisplayName',mstr)
-    cont=cont+1;
+    plot(medias(1,aux),medias(2,aux),'+','MarkerSize',12,'DisplayName',mstr,'color',rand(1,3))
 end
 xlabel('Atributo X')
 ylabel('Atributo Y')
