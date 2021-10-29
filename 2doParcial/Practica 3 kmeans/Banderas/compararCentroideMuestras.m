7e436080e84446d68c78ca5f42b80a5cd6fd9307
666mlt <fermatadamas.l@gmail.com>
2021-10-29 01:50:18 -0500

    Abre primero Imagen

--- 2doParcial/Practica 3 kmeans/Banderas/compararCentroideMuestras.m
+++
@@ -1,9 +0,0 @@
-function bandera=compararCentroideMuestras(centroides,muestra,n)
-    bandera=1; %Se puede agregar como nuevo centroide la muestra
-    for i=1:n
-        if muestra==centroides(:,i)
-            bandera=0;
-            break
-        end
-    end
-end
