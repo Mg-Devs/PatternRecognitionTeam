e1dd68b83125cbaab5d456bcc10d7821c5e8df44
AngelPLJ <64231690+AngelPLJ@users.noreply.github.com>
2021-11-30 14:08:21 -0600

    Practica 2 completa

--- 3erParcial/Practica2/defObjeto.m
+++ 3erParcial/Practica2/defObjeto.m
@@ -1,18 +1,18 @@
 function tipoObj = defObjeto(centroide)
     %Los tipo 1 son Tornillos
-    if (centroide(1) > 36 && centroide(1) < 60) && (centroide(2) > 100 && centroide(2) < 150)
+    if (centroide(1) > 40 && centroide(1) < 60) && (centroide(2) > 50 && centroide(2) < 60)
         tipoObj = 1;
     
     %Los tipo 2 son Llaves
-    elseif (centroide(1) > 42 && centroide(1) < 80) && (centroide(2) > 200 && centroide(2) < 250)
+    elseif (centroide(1) > 45 && centroide(1) < 65) && (centroide(2) > 90 && centroide(2) < 110) 
         tipoObj = 2;
     
     %Los tipo 3 son Armellas
-    elseif (centroide(1) > 60 && centroide(1) < 80) && (centroide(2) > 150 || centroide(2) < 182)
+    elseif (centroide(1) > 60 && centroide(1) < 80) && (centroide(2) > 70 && centroide(2) < 80)
         tipoObj = 3;
     
     %Los tipo 4 son Rondanas
-    elseif (centroide(1) > 75 && centroide(1) < 95) && (centroide(2) > 85 && centroide(2) < 100)
+    elseif (centroide(1) > 80 && centroide(1) < 100) && (centroide(2) > 30 && centroide(2) < 40)
         tipoObj = 4;
     
     else
